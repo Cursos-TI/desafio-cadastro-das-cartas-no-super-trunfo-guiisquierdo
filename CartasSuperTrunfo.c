6e9ce4c765cb0ade30a66e0dd7b9b790989fcb22
guiisquierdo <guiisquierdo@gmail.com>
2025-03-25 00:36:04 +0000

    coclusao desafio nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,6 +3,68 @@
 int main      (){
 
     printf("Desafio super trunfo \n");
+
+
+
+    printf("carta 01 \n");
+
+    char    Estado1[20] = "Alagoas";
+    char    codigo1 [20] = "A01";
+    char     Cidade1[20] = "Maceio";
+    int habitantes1  = 950000;
+    float   tamanho1 = 510.000;
+    float      PIB1 = 77000.000;
+    int turismo1 = 20;
+
+    printf("Estado: %s\n", Estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Cidade: %s\n", Cidade1);
+    printf("populaçao: %d\n", habitantes1);
+    printf("Tamanho: %f\n", tamanho1);
+    printf("PIB: %f\n", PIB1);
+    printf("Pontos turisticos: %d\n", turismo1);
+
+
+
+    printf("Carta 02 \n");
+
+    char    estado2[20] = "Ceara";
+    char    codigo2[20] = "B01";
+    char    cidade2[20] = "Fortaleza";
+    int habitantes2 = 2.500000;
+    float   tamanho2 = 312.000;
+    float   PIB2 = 75.00000;
+    int turismo2 = 15;
+
+    printf("Estado: %s\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("populaçao: %d\n", habitantes2);
+    printf("Tamanho: %f\n", tamanho2);
+    printf("PIB: %f\n", PIB2);
+    printf("Pontos turisticos: %d\n", turismo2);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
     
 
 
@@ -13,8 +75,3 @@ int main      (){
 
 
 
-
-
-
-return 0;
-}
